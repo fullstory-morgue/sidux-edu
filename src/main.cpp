e54b408b06b06474e38da3e0075e68d85e57c345
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-11-29 13:32:43 +0000

    * fix FSF address.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  * main.cpp
  *
- * Copyright (c) 2007 Fabian Wuertz
+ * Copyright (c) 2007 Fabian Wuertz <xadras@sidux.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #include <systray.h>
